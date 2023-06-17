48c4d43e07372161163497dce7b25c3a11866752
Estyk1 <estherkyei06@gmail.com>
2023-06-17 07:10:15 -0700

    puts prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-/* *
- *
+/**
  * main -it prints out output using put
  *
- * Return: always (0)
+ * Return: Always (0)
  *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
